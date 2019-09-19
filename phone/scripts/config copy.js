1cbc1bfae8bb0cec3de84589744f0f4e4e232c7c
ringneck <norman.southcastle@gmail.com>
2019-09-19 22:46:09 +0900

    First Upload
    First Upload

---
+++ phone/scripts/config copy.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "1010",
+    //  Password
+    "Pass" : "olssoo3838i",
+    //  Auth Realm
+    "Realm"   : "webphone.olssoo.com",
+    // Display Name
+    "Display" : "WebPhone",
+    // WebSocket URL
+    "WSServer"  : "wss://webphone.olssoo.com:7443"
+};
