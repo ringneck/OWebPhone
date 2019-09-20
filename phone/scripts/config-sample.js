fe8046b6dc1a8fe27833d4c97fb9866d9abfeae9
ringneck <norman.southcastle@gmail.com>
2019-09-20 11:42:58 +0900

    Add config 입력가이드
    Add config 입력가이드

--- phone/scripts/config-sample.js
+++ phone/scripts/config-sample.js
@@ -1,12 +1,14 @@
+//  주식회사 얼쑤팩토리 VPBX 신청 후 발급받은 정보를 입력합니다. 
+
 var user = {
-    //  User Name
+    //  SIP 사용자ID
     "User" : "sipuser",
-    //  Password
+    //  SIP 비밀번호
     "Pass" : "yourpassword",
-    //  Auth Realm
-    "Realm"   : "sip.sample.com",
-    // Display Name
-    "Display" : "Gob Bleuth",
-    // WebSocket URL
-    "WSServer"  : "wss://wss.sample.com:8443"
+    //  SIP Auth Realm 
+    "Realm"   : "realm.olssoo.com",
+    // 발신자 표시이름
+    "Display" : "olssoo",
+    // WebSocket 서버 URL
+    "WSServer"  : "wss://wss.olssoo.com:8443"
 };
